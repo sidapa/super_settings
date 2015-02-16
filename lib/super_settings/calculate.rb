4191bacf0d2251a3ad46e61e31fa7fd252ed2d43
Adrian Joseph Tumusok <me@adriantumusok.com>
2015-02-16 22:14:49 +0800

    Add .register to SuperSettings::Calculate

--- lib/super_settings/calculate.rb
+++ lib/super_settings/calculate.rb
@@ -1,4 +1,11 @@
 module SuperSettings
   class Calculate
+    @operators = {}
+
+    class << self
+      def register(operator, receiver)
+        @operators[operator] = receiver
+      end
+    end
   end
 end
--- spec/super_settings/calculate_spec.rb
+++ spec/super_settings/calculate_spec.rb
@@ -1,10 +1,34 @@
 require 'spec_helper'
 
 describe SuperSettings::Calculate do
+  before(:each) do
+    # put registry in a temporary variable
+    @operators_value_hash = SuperSettings::Calculate
+                              .instance_variable_get('@operators')
+    SuperSettings::Calculate.instance_variable_set('@operators', Hash.new)
+  end
+
+  after(:each) do
+    # return registry values
+    SuperSettings::Calculate
+      .instance_variable_set('@operators', @operators_value_hash)
+  end
+
   context '.method_missing' do
     it 'should call a registered operator if no method was found'
   end
+
   context '.register' do
-    it 'should allow an operator to be registered'
+    let(:registered_operator) { { klass: 'test', method: 'only' } }
+
+    it 'should allow an operator to be registered' do
+      SuperSettings::Calculate.register(:lookup_key, registered_operator)
+
+      lookup_result = SuperSettings::Calculate
+                      .instance_variable_get('@operators')[:lookup_key]
+      expect(lookup_result).to eql(registered_operator)
+    end
+
+    it 'should fail if operator has been registered'
   end
 end
