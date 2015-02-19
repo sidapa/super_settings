2abb8122b9bba13cdab68166ced021fb5985e235
Adrian Joseph Tumusok <adrian@egissys.com>
2015-02-19 11:49:34 +0800

    Rubocop compliance fixes

--- lib/super_settings.rb
+++ lib/super_settings.rb
@@ -1,5 +1,8 @@
 require 'super_settings/rule_registry'
 require 'super_settings/calculate'
 
+# This is the main SuperSettings module.
+# SuperSettings allows a user to define
+# Backend agnostic settings, rules and operations
 module SuperSettings
 end
