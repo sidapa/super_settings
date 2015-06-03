a0959118fd6e46545d515bd775eb1e23b990762d
Adrian Joseph Tumusok <me@adriantumusok.com>
2015-06-03 21:16:36 +0800

    Allow a FeatureSet to be configured (WIP)
    Will allow FeatureSet to be configured to have persistence (and specify
    the backend via adapter (maybe?) as well as other configuration details.

--- lib/super_settings/feature_set.rb
+++ lib/super_settings/feature_set.rb
@@ -1,3 +1,5 @@
+require 'ostruct'
+
 module SuperSettings
   # This class allows for feature toggles
   class FeatureSet
@@ -5,5 +7,23 @@ module SuperSettings
     extend SuperSettings::AllowMethodMissingOverride
 
     # TODO: create a class that deals with contexts
+
+    class << self
+      attr_accessor :configuration
+
+      def config
+        @configuration ||= OpenStruct.new
+        yield @configuration
+        parse_configuration
+      end
+
+      def parse_configuration
+        # Do nothing for now. Maybe iterate over the
+        # different parsers and pass configuration into those
+        # those parsers would then perfom their own checks on
+        # the configuration object. Or maybe create a new
+        # configuration object completely?
+      end
+    end
   end
 end
--- spec/super_settings/feature_set_spec.rb
+++ spec/super_settings/feature_set_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe SuperSettings::FeatureSet do
+  describe '::config' do
+    subject(:configuration) { SuperSettings::FeatureSet.configuration }
+
+    before(:each) do
+      SuperSettings::FeatureSet.config do |config|
+        config.foo = :bar
+      end
+    end
+
+    it { expect(configuration.foo).to eql(:bar) }
+  end
 end
