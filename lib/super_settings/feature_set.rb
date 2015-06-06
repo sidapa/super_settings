3d702c3308853115064b2e4b800154396cae6716
Adrian Joseph Tumusok <me@adriantumusok.com>
2015-06-06 10:04:05 +0800

    Add post processing of configuration via parsers

--- lib/super_settings/feature_set.rb
+++ lib/super_settings/feature_set.rb
@@ -2,28 +2,31 @@ require 'ostruct'
 
 module SuperSettings
   # This class allows for feature toggles
-  class FeatureSet
+  module FeatureSet
     extend SuperSettings::AllowRegistration
     extend SuperSettings::AllowMethodMissingOverride
 
     # TODO: create a class that deals with contexts
 
-    class << self
-      attr_accessor :configuration
+    module_function
 
-      def config
-        @configuration ||= OpenStruct.new
-        yield @configuration
-        parse_configuration
-      end
+    @parsers = []
 
-      def parse_configuration
-        # Do nothing for now. Maybe iterate over the
-        # different parsers and pass configuration into those
-        # those parsers would then perfom their own checks on
-        # the configuration object. Or maybe create a new
-        # configuration object completely?
+    def config
+      @configuration ||= OpenStruct.new
+      yield @configuration
+      parse_configuration
+    end
+
+    def parse_configuration
+      @parsers.uniq.each do |parser|
+        parser.call(@configuration)
       end
+    end
+
+    def add_config_parser(parser)
+      fail Error, 'call method required' unless parser.respond_to? :call
+      @parsers << parser
     end
   end
 end
--- spec/super_settings/feature_set_spec.rb
+++ spec/super_settings/feature_set_spec.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 
 describe SuperSettings::FeatureSet do
+  let(:parser) do
+    # This is just a sample class
+    class DoubleTest
+      def self.call
+        true
+      end
+    end
+    DoubleTest
+  end
+
   describe '::config' do
-    subject(:configuration) { SuperSettings::FeatureSet.configuration }
+    subject(:configuration) do
+      SuperSettings::FeatureSet.instance_variable_get(:@configuration)
+    end
 
     before(:each) do
       SuperSettings::FeatureSet.config do |config|
@@ -11,5 +23,48 @@ describe SuperSettings::FeatureSet do
     end
 
     it { expect(configuration.foo).to eql(:bar) }
+    it 'parses the configuration' do
+      expect(SuperSettings::FeatureSet).to receive(:parse_configuration)
+
+      SuperSettings::FeatureSet.config do |config|
+        config.foo = :bar
+      end
+    end
+  end
+
+  describe '::add_config_parser' do
+    subject(:add_parser) { SuperSettings::FeatureSet.add_config_parser(parser) }
+    let(:parsers) do
+      SuperSettings::FeatureSet.instance_variable_get(:@parsers)
+    end
+
+    context 'Parser size' do
+      it { expect { add_parser }.to change { parsers.size }.by(1) }
+    end
+
+    context 'Parser content' do
+      it { expect(parsers).to eql([parser]) }
+    end
+
+    context 'Added parser does not support the method ::call' do
+      let(:parser) { String }
+
+      it { expect { add_parser }.to raise_error SuperSettings::Error }
+    end
+  end
+
+  describe '::parse_configuration' do
+    before(:each) { SuperSettings::FeatureSet.add_config_parser(parser) }
+
+    let(:configuration) do
+      SuperSettings::FeatureSet.instance_variable_get(:@configuration)
+    end
+
+    subject(:run_parse) { SuperSettings::FeatureSet.parse_configuration }
+
+    it 'calls the call method on each of the parsers' do
+      expect(DoubleTest).to receive(:call).with(configuration)
+      run_parse
+    end
   end
 end
