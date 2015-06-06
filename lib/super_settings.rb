12500dfe941422b016385eae3d49b92807e302d6
Adrian Joseph Tumusok <me@adriantumusok.com>
2015-06-06 11:38:35 +0800

    Auto require files

--- lib/super_settings.rb
+++ lib/super_settings.rb
@@ -1,10 +1,8 @@
-require 'super_settings/allow_registration'
-require 'super_settings/allow_method_missing_override'
-require 'super_settings/exceptions'
-require 'super_settings/rule_key_parser'
-require 'super_settings/rule_registry'
-require 'super_settings/feature_set'
-require 'super_settings/feature'
+current_directory = File.dirname(__FILE__)
+
+%w(/super_settings/*.rb /super_settings/feature_set_parsers/*.rb).each do |dir|
+  Dir[current_directory + dir].each { |file| require file }
+end
 
 # This is the main SuperSettings module.
 # SuperSettings allows a user to define
