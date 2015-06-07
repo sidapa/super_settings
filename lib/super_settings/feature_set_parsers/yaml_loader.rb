43de3a39bef630b5d58c0117703aa049e37b3eba
Adrian Joseph Tumusok <me@adriantumusok.com>
2015-06-07 23:29:50 +0800

    Load SuperSettings::FeatureSet with data from YAML file

--- lib/super_settings/feature_set_parsers/yaml_loader.rb
+++ lib/super_settings/feature_set_parsers/yaml_loader.rb
@@ -4,20 +4,43 @@ require 'json'
 module SuperSettings
   module FeatureSetLoaders
     # Allows FeatureSet to load data from a yml file
-    module YamlLoader
-      module_function
-
-      def call(configuration)
+    class YamlLoader
+      def self.call(configuration)
         return false unless configuration.datastore == :yaml
 
-        fail Error if configuration.filename.nil?
+        filename = configuration.filename
+        fail Error if filename.nil?
+
+        new(filename).load_data
+      end
+
+      def initialize(filename)
+        fail Error, "#{filename} not found." unless File.exist?(filename)
+
+        @filename = filename
+      end
+
+      def load_data
+        features_json.each_pair do |k, v|
+          feature = SuperSettings::Feature.new(symbolize_hash(v))
+          SuperSettings::FeatureSet.add_feature(k.to_sym, feature)
+        end
+
         true
       end
 
-      def features_json(filename)
-        json_data = YAML.load(filename)
+      def features_json
+        json_data = YAML.load(@filename)
 
         JSON.parse(json_data)
+      end
+
+      private
+
+      def symbolize_hash(hash_value)
+        hash_value.each_with_object({}) do |(key, value), memo|
+          memo[key.to_sym] = value
+        end
       end
     end
   end
--- spec/super_settings/feature_set_parsers/yaml_loader_spec.rb
+++ spec/super_settings/feature_set_parsers/yaml_loader_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 require 'ostruct'
 
 describe SuperSettings::FeatureSetLoaders::YamlLoader do
+  let(:filename) { 'test' }
+
+  before(:each) do
+    allow(File).to receive(:exist?).with(filename).and_return true
+  end
+
   describe '::call' do
     subject(:call_method) do
       SuperSettings::FeatureSetLoaders::YamlLoader.call(configuration)
@@ -11,10 +17,26 @@ describe SuperSettings::FeatureSetLoaders::YamlLoader do
       OpenStruct.new(datastore: :yaml, filename: filename)
     end
 
-    let(:filename) { 'test' }
+    let(:json_data) do
+      { feature_name:
+        {
+          default: true,
+          foo: 'bar'
+        }
+      }.to_json
+    end
 
     context 'complete data' do
+      before(:each) do
+        allow(YAML).to receive(:load).and_return(json_data)
+      end
+
+      let(:feature_set) do
+        SuperSettings::FeatureSet
+      end
+
       it { expect(call_method).to eql(true) }
+      it { expect(feature_set.respond_to? :feature_name).to eql(true) }
     end
 
     context 'not configured' do
@@ -30,19 +52,35 @@ describe SuperSettings::FeatureSetLoaders::YamlLoader do
     end
   end
 
-  describe '::features_json' do
-    subject(:features_json_method) do
-      SuperSettings::FeatureSetLoaders::YamlLoader.feature_json(filename)
+  describe '::new' do
+    before(:each) do
+      allow(File).to receive(:exist?).with(filename).and_return false
     end
 
-    let(:filename) { 'test' }
+    subject(:loader_instance) do
+      SuperSettings::FeatureSetLoaders::YamlLoader.new(filename)
+    end
+
+    context 'file does not exist' do
+      it 'fails if the file does not exist' do
+        expect { loader_instance }.to raise_error SuperSettings::Error
+      end
+    end
+  end
+
+  describe '#features_json' do
+    subject(:loader_instance) do
+      SuperSettings::FeatureSetLoaders::YamlLoader.new(filename)
+    end
+
+    let(:json_double) { double }
 
     context 'complete data' do
       it 'creates a JSON object from the YAML' do
         expect(YAML).to receive(:load).with(filename)
-        expect(JSON).to receive(:parse)
+        expect(JSON).to receive(:parse).and_return json_double
 
-        SuperSettings::FeatureSetLoaders::YamlLoader.features_json(filename)
+        expect(loader_instance.features_json).to eql(json_double)
       end
     end
   end
