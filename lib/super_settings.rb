3233ec0586a736d78eddb468326b1cd7ed8ad153
Adrian Joseph Tumusok <me@adriantumusok.com>
2015-06-08 08:11:21 +0800

    Load all ruby files automatically

--- lib/super_settings.rb
+++ lib/super_settings.rb
@@ -1,7 +1,5 @@
-current_directory = File.dirname(__FILE__)
-
-%w(/super_settings/*.rb /super_settings/feature_set_parsers/*.rb).each do |dir|
-  Dir[current_directory + dir].each { |file| require file }
+Dir["#{File.dirname(__FILE__)}/super_settings/**/*.rb"].each do |file|
+  require(file)
 end
 
 # This is the main SuperSettings module.
